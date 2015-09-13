918ca7e8bcc0c99818b85d38c1094dd0b27c327d
Matt Osborne <osbornematt1@gmail.com>
2015-09-13 20:51:33 +1000

    Added logic to parse hyphenated and non hyphenated filenames to the same directory

--- comic-organiser.ps1
+++ comic-organiser.ps1
@@ -46,12 +46,35 @@ $Folders = Get-ChildItem  -Path "$SortDirectory" -Directory -recurse | Select -E
 #For each Directory, search for comic book titles that matches, and move them there.
  ForEach($item in $Folders)
     {
+        
+            $temp = split-path $item -Leaf
 
-      $temp = split-path $item -Leaf
-      Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbz -include *$temp* -recurse  | Move-Item -Destination $item -Force 
-      Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbr -include *$temp* -recurse | Move-Item -Destination $item -Force
-      Get-ChildItem -Path "$DumpDirectory" -File -filter *.pdf -include *$temp* -recurse | Move-Item -Destination $item -Force
+          if ($temp.Contains("-"))
+          {
+                    Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbr -include *$temp* -recurse  | Move-Item -Destination $item -Force 
+                
+                    Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbz -include *$temp* -recurse  | Move-Item -Destination $item -Force 
+                
+                    Get-ChildItem -Path "$DumpDirectory" -File -filter *.pdf -include *$temp* -recurse  | Move-Item -Destination $item -Force
+        
+                    $temp2 = $temp.Replace("-"," ")
+
+                    Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbr -include *$temp2* -recurse  | Move-Item -Destination $item -Force 
+                
+                    Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbz -include *$temp2* -recurse  | Move-Item -Destination $item -Force 
+                
+                    Get-ChildItem -Path "$DumpDirectory" -File -filter *.pdf -include *$temp2* -recurse  | Move-Item -Destination $item -Force
       
+        
+          }
+          else
+          {
+                  Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbz -include *$temp* -recurse | Move-Item -Destination $item -Force 
+              
+                  Get-ChildItem -Path "$DumpDirectory" -File -filter *.cbr -include *$temp* -recurse | Move-Item -Destination $item -Force
+              
+                  Get-ChildItem -Path "$DumpDirectory" -File -filter *.pdf -include *$temp* -recurse | Move-Item -Destination $item -Force
+          }
 
     }
   
